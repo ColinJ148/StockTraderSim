93926fa4c2ca93a32d3661d013cb0570b8afab93
Colin Joyce <colin.joyce9@gmail.com>
2018-11-20 15:27:30 -0500

    Updating StockQuote

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,7 @@
 package sample;
 
-import java.util.Date;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
 
@@ -25,21 +21,69 @@ public class Controller {
   @FXML
   private TextField userShareAmt;       //Amount of shares users wants to buy or sell
   @FXML
-  private Text stock1;
+  private Text stockOneName;
   @FXML
-  private Text stock2;
+  private Text stockOnePrice;
   @FXML
-  private Text stock3;
+  private Text stockOnePriceChange;
   @FXML
-  private Text stock4;
+  private Text stockTwoName;
+  @FXML
+  private Text stockTwoPrice;
+  @FXML
+  private Text stockTwoPriceChange;
+  @FXML
+  private Text stockThreeName;
+  @FXML
+  private Text stockThreePrice;
+  @FXML
+  private Text stockThreePriceChange;
+  @FXML
+  private Text stockFourName;
+  @FXML
+  private Text stockFourPrice;
+  @FXML
+  private Text stockFourPriceChange;
 
 
   private double cash = 10000;
   private double portfolioValue = 0;
   private double networth = 10000;
   private double orderTotal;
+  private String[] price = new String[10];
 
   public Controller() {
+
+  }
+
+
+  @FXML
+  private void updatePrices() {
+    StockQuote tsla = new StockQuote("TSLA");
+    StockQuote ge = new StockQuote("GE");
+    StockQuote snap = new StockQuote("SNAP");
+    StockQuote amd = new StockQuote("AMD");
+
+    stockOneName.setText("Tesla (TSLA)");
+    stockOnePrice.setText(tsla.getPrice());
+    stockOnePriceChange.setText(tsla.getPriceChange());
+    price[0]=tsla.getPrice();
+
+    stockTwoName.setText("General Electric (GE)");
+    stockTwoPrice.setText(ge.getPrice());
+    stockTwoPriceChange.setText(ge.getPriceChange());
+    price[1]=ge.getPrice();
+
+    stockThreeName.setText("Snapchat (SNAP)");
+    stockThreePrice.setText(snap.getPrice());
+    stockThreePriceChange.setText(snap.getPriceChange());
+    price[2]=snap.getPrice();
+
+    stockFourName.setText("Advanced Micro Devices (AMD)");
+    stockFourPrice.setText(amd.getPrice());
+    stockFourPriceChange.setText(amd.getPriceChange());
+    price[3]=amd.getPrice();
+
 
   }
 
@@ -52,11 +96,16 @@ public class Controller {
     } else {
       ErrorCode.setText("Not enough Cash for order");
     }
-    netWorthCalc(cash, portfolioValue);
+    netWorthCalc();
   }
+
+
 
   private void orderInfo() {
     String ticker = null;
+    String tickerSymbols[] = {"TSLA", "GE", "SNAP", "AMD"};
+    String value=null;
+
     int shareAmt = 0;
     try {
       ticker = userTickerSymbol.getText();
@@ -64,35 +113,34 @@ public class Controller {
     } catch (NumberFormatException e) {
       ErrorCode.setText("Please enter valid Share amount");
     }
-    orderTotal = shareAmt * 20;
-    //get ticker price from api
 
-//    System.out.println(ticker);
-//    System.out.println("Share Amt:" + shareAmt);
+    for (int i = 0; i < tickerSymbols.length; i++) {
+      if (ticker.equals(tickerSymbols[i])) {
+        ticker = tickerSymbols[i];
+        value = price[i];
+        orderTotal = shareAmt * Double.valueOf(value);
+      }
+    }
 
+
+ //   ErrorCode.setText("Please enter valid ticker Symbol");
   }
+
+
+
+
+
   @FXML
-  private void updatePrices(){
-    StockQuote tsla = new StockQuote("TSLA");
-    StockQuote ge = new StockQuote("GE");
-    StockQuote snap = new StockQuote("SNAP");
-    StockQuote amd = new StockQuote("AMD");
-
-    stock1.setText(tsla.getPrice() + "      " + tsla.getPriceChange());
-  //  stock2.setText(ge.getPrice() + "      " + ge.getPriceChange());
-
-
+  public void onStockSell() {
 
   }
-  @FXML
-  public void onStockSell(){
 
-  }
-  private void netWorthCalc(Double cash, Double portfolioValue) {
+  private void netWorthCalc() {
     networth = cash + portfolioValue;
-    netWorth.setText(String.valueOf(networth));
-    Cash.setText(String.valueOf(cash));
-    portValue.setText(String.valueOf(portfolioValue));
+    netWorth.setText(Double.toString(networth));
+    Cash.setText("10000");
+    portValue.setText(Double.toString(portfolioValue));
+
 
 
   }
--- src/sample/StockQuote.java
+++ src/sample/StockQuote.java
@@ -6,8 +6,13 @@ import java.util.Scanner;
 class StockQuote {
 
   private String content;
+  private String companyName;
+  private String ticker;
+  private String historyContent;
+
 
   public StockQuote(String ticker) {
+    this.ticker = ticker;
     String content = null;
     URLConnection connection;
     try {
@@ -19,41 +24,83 @@ class StockQuote {
     } catch (Exception ex) {
       ex.printStackTrace();
     }
-
+    getPriceHistory();
   }
 
+  public void getPriceHistory() {
+    String html;
+    URLConnection connection;
+    try {
+      connection = new URL("https://finance.yahoo.com/quote/AAPL/history?p=" + ticker).openConnection();
+      Scanner scanner = new Scanner(connection.getInputStream());
+      scanner.useDelimiter("\\Z");
+      historyContent = scanner.next();
+      this.historyContent = historyContent;
+    } catch (Exception e) {
+      System.out.println("Unable to get history");
+    }
+//    System.out.println(historyContent);
+  }
+
+  private void parseHistory() {
+    String snipit;
+    String reactId;
+    int indexOfReactId;
+    int index = historyContent.indexOf("Nov 16, 2018")+71;
+    reactId = historyContent.substring(index,index+5);
+    reactId = reactId.replaceAll("[^0-9]","");
+    indexOfReactId = Integer.parseInt(reactId) + 7;
+
+
+    index = historyContent.indexOf(indexOfReactId);
+
+
+    snipit= historyContent.substring(index,index+10);
+    System.out.println(snipit);
+
+
+    System.out.println(reactId);
+    System.out.println(index);
+  }
 
   public String getPrice() {
     String price;
     String snipit;
 
-
     int index = content.indexOf("regularMarketVolume") - 4;
     snipit = content.substring(index - 7, index);
     for (int i = 0; i < snipit.length(); i++) {
-      snipit = snipit.replaceAll("[:]","");
+      snipit = snipit.replaceAll("[:]", "");
       snipit = snipit.replaceAll("^\"|\"$", "");
     }
-
     price = snipit;
     return price;
   }
 
-  public String getPriceChange(){
+  public String getPriceChange() {
     String priceChange;
     String snipit;
     int index = content.indexOf("fullExchangeName") - 4;
     snipit = content.substring(index - 7, index);
     for (int i = 0; i < snipit.length(); i++) {
-      snipit = snipit.replaceAll("[:]","");
+      snipit = snipit.replaceAll("[:]", "");
       snipit = snipit.replaceAll("^\"|\"$", "");
     }
     priceChange = snipit;
     return priceChange;
   }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
   public static void main(String[] args) {
-    StockQuote tesla = new StockQuote("TSLA");
-    System.out.println(tesla.getPrice());
+    StockQuote test = new StockQuote("AAPL");
+    test.parseHistory();
   }
 
 }
